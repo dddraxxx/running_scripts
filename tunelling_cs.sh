088d378d4a41e2809b45516de3d505b22bb71e55
Drax <52069185+dddraxxx@users.noreply.github.com>
2025-04-23 18:17:52 -0400

    Update tunelling_cs.sh

--- tunelling_cs.sh
+++ tunelling_cs.sh
@@ -52,7 +52,7 @@ echo "Wandb monitoring started with PID: $WANDB_PID"
 curl -Lk 'https://api2.cursor.sh/updates/download-latest?os=cli-alpine-x64' --output vscode_cli.tar.gz
 
 tar -xf vscode_cli.tar.gz
-./cursor tunnel --no-sleep --accept-server-license-terms --install-extension ms-python.python --install-extension ms-toolsai.jupyter --install-extension eamodio.gitlens | tee vscode_tunnel.log
+./cursor tunnel --no-sleep --accept-server-license-terms --install-extension ms-python.python --install-extension ms-toolsai.jupyter --install-extension eamodio.gitlens --install-extension kisstkondoros.vscode-gutter-preview | tee vscode_tunnel.log
 
 wait $WANDB_PID
 # When tunneling ends, stop the wandb monitor
