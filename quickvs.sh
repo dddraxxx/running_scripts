dc45b91172c0bfd65a018662117b0be13d8b12a0
Drax <52069185+dddraxxx@users.noreply.github.com>
2026-04-11 00:32:29 -0400

    Redirect tunnel command output to log file
    Log the output of the tunnel command to vscode_tunnel.log.

--- quickvs.sh
+++ quickvs.sh
@@ -1,4 +1,10 @@
+
+touch vscode_tunnel.log
+
+# Continue with normal tunneling process
 curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz
 
 tar -xf vscode_cli.tar.gz
-./code tunnel --accept-server-license-terms
+./code tunnel --accept-server-license-terms | tee vscode_tunnel.log
+
+sleep infinity
