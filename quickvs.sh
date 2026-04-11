79e9e75e8941c6dd8b5f6608635bcf2449e9d9f3
Drax <52069185+dddraxxx@users.noreply.github.com>
2026-04-11 00:37:06 -0400

    Install Python extension during VS Code tunnel setup

--- quickvs.sh
+++ quickvs.sh
@@ -5,6 +5,6 @@ touch vscode_tunnel.log
 curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz
 
 tar -xf vscode_cli.tar.gz
-./code tunnel --accept-server-license-terms | tee vscode_tunnel.log
+./code tunnel --accept-server-license-terms --install-extension ms-python.python  | tee vscode_tunnel.log
 
 sleep infinity
